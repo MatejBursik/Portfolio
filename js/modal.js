f2cbcad97739d2cd6d67d1e52c05c8df466207ed
MatejBursik <r0929146@student.thomasmore.be>
2025-05-26 13:18:31 +0200

    changed how the modals in projects work, the inside of the model nolonger has a strict structure

--- js/modal.js
+++ js/modal.js
@@ -1,70 +1,36 @@
-let mTitle = document.getElementById('title');
-let mDescription = document.getElementById('description');
-let mReason = document.getElementById('reason');
-let mWIL = document.getElementById('wil');
-let mWILH3 = document.getElementById('wilH3');
-let mSource = document.getElementById('source');
-let mImage = document.getElementById('images');
-
 let mExit = document.getElementById('modal-exit-btn');
 let mBKG = document.getElementById('model-bkg');
-let modal = document.getElementById('modal');
-let show = document.getElementsByClassName("modal-show");
+let modal = document.getElementsByClassName("modal"); // all modals
+let modal_show = document.getElementsByClassName("modal-show"); // all boxes to show modals
 let click;
 
-let info
-fetch('../data/projects.json')
-    .then(response => {return response.json();})
-    .then(data => {
-        info = data;
-    })
-    .catch(error => {console.error('There was a problem with the fetch operation:', error);});
+let info;
 
-for (let i=0; i < show.length; i++){
-    show[i].addEventListener('click', function(e) {
-        modal.style.display = "block";
-        mBKG.style.display = "block";
+for (let i=0; i < modal_show.length; i++){
+    modal_show[i].addEventListener('click', function(e) {
+        console.log(modal_show[i].id)
+        let targetModal = Array.from(modal)
+                       .find(m => m.id === modal_show[i].id + "_modal");
 
-        proj = info[show[i].id];
-        mTitle.innerText = proj.title;
-        mDescription.innerText = proj.description;
-        mReason.innerText = proj.reason;
-        if(proj.wil === "none"){
-            mWIL.style.display = "none";
-            mWILH3.style.display = "none";
-        } else {
-            mWIL.innerText = proj.wil;
-            mWIL.style.display = "block";
-            mWILH3.style.display = "block";
-        }
-        switch(proj.source.type){
-            case "github":
-                mSource.innerHTML = '<a href="' + proj.source.href + '" target="_blank">Link to github</a>';
-                break;
-            case "report":
-                mSource.innerHTML = '<a href="' + proj.source.href + '" download>Download the report</a>';
-                break;
-        }
-        mImage.innerHTML = '';
-        for(let j=0; j<proj.images.count; j++){
-            mImage.innerHTML += '<img src="' + proj.images.src[j] + '">';
-            if(j < proj.images.count - 1){
-                mImage.innerHTML += '<h4 style="text-align: center; margin-bottom: 25px;">' + proj.images.desc[j] + '</h4>';
-            } else {
-                mImage.innerHTML += '<h4 style="text-align: center;">' + proj.images.desc[j] + '</h4>';
-            }
-        }
+        if (targetModal) {
+            targetModal.style.display = "block";
+            mBKG.style.display = "block";
 
-        modal.scrollTop = 0;
+            modal.scrollTop = 0;
+        }
     });
 }
 
 mBKG.addEventListener('click', function(e) {
-    modal.style.display = "none";
+    for (let i=0; i < modal.length; i++){
+        modal[i].style.display = "none";
+    }
     mBKG.style.display = "none";
 });
 
 mExit.addEventListener('click', function(e) {
-    modal.style.display = "none";
+    for (let i=0; i < modal.length; i++){
+        modal[i].style.display = "none";
+    }
     mBKG.style.display = "none";
 });
